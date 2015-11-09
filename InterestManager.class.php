95d9e13b6ced4a9d9e2983e6e3eac036337692e7
Merit Paist <meripai@greeny.cs.tlu.ee>
2015-11-09 15:15:09 +0200

    tunnitöö

--- InterestManager.class.php
+++ InterestManager.class.php
@@ -58,13 +58,91 @@ class InterestManager {
         return $response;
         
     }
-		
-		
-		//kui ei ole, lisad uue
-		
-		
-		
 	
+	function createDropdown(){
+	  $html = '';		
+		//lisan valikud
+	  $html = '<select name="dropdown interest">';	
+//// see lause tuleb teha nii, et näitaks ainult neid, mida kasutaja ei ole valinud.
+	  $stmt=$this->connection->prepare("SELECT interests.id, interests.name, user_interests.id FROM interests 
+	  JOIN user_interests ON WHERE user_interests.interests_id != ")
+	  $stmt->bind_result($id, $name);
+	  $stmt->execute();
+	  
+	  while($stmt->fetch()){
+		   $html. = '<option value="'$id'">'.$name.'</option>';	
+		  
+	  }
+	  
+	    $stmt->close();
+	  
+	  //$html = '<option>Test 1</option>';	
+	  //$html = '<option selected>Test 2</option>';	
+	  $html .= '</select>';	
+
+     return $html;	  
+	}
+	
+	function addUserInterest($interests_id){
+		//kontrollime, et sellist veel ei ole
+		
+		$response = new StdClass();
+        
+        $stmt = $this->connection->prepare("SELECT id FROM user_interests WHERE interests_id = ?");
+        $stmt->bind_param("i", $interests_id);
+        $stmt->execute();
+        
+        // kas saime rea andmeid
+        if($stmt->fetch()){
+            
+            // huviala on juba olemas
+            $error = new StdClass();
+            $error->id = 0;
+            $error->message = "Huviala '".$name."' on juba olemas";
+            
+            $response->error = $error;
+            
+            // pärast return käsku, fn'i enam edasi ei vaadata
+            return $response;
+            
+        }
+        $stmt->close();
+        //siia olen jõudnud siis kui huviala ei olnud
+        $stmt = $this->connection->prepare("INSERT INTO user_interests (user_id, interests_id) VALUES (?,?)");
+        $stmt->bind_param("i,i", $this->user_id, $interests_id);
+        if($stmt->execute()){
+            // sisestamine õnnestus
+            $success = new StdClass();
+            $success->message = "Huviala lisatud";
+            
+            $response->success = $success;
+            
+        }else{
+            //ei õnnestunud
+            $error = new StdClass();
+            $error->id = 1;
+            $error->message = "Midagi läks katki";
+            
+            $response->error = $error;
+        }
+        $stmt->close();
+        
+        return $response;
+        
+    }
+		
+	function GetUserInterest();
+		//saada kätte ja saata tagasi kõik kasutaja huvialad
+		//kasutaja id $this->user_id
+		//kõik tema huvialade nimed
+		
+	 $stmt = $this->connection->prepare("SELECT interests.name FROM user_interests INNER JOIN interests on user_interests.interests_id=interests_id WHERE user_interests_user_id=?");
+     $stmt->bind_param("i", $this->user_id);
+	 $stmt->bind_result($name);
+	 $stmt->fetch()){
+		echo $name." <br>"; 
+		 
+	 }
 	
 }
 ?>
--- data.php
+++ data.php
@@ -17,8 +17,12 @@
     //*************HALDUS
 	$InterestManager=new InterestManager($mysqli, $_SESSION['id_from_db']);
 	
-	if(!isset($_GET['new_interest'])){
+	if(isset($_GET["new_interest"])){
 		$add_interest_response = $InterestManager->addInterest($_GET["new_interest"]);
+		}
+	// siin on mingi jama	
+	if(isset($_GET["dropdown_interest"])){
+		$add_user_interest_response = $InterestManager->addUserInterest($_GET["dropdown_interest"]);
 		}
 ?>
 	
@@ -44,8 +48,30 @@ Tere, <?=$_SESSION['user_email'];?> <a href="?logout=1">Logi välja</a>
 <?php endif; ?>  
 
 <form>
-  	<input name="new interest" 
+  	<input name="new interest" > <br>
   	  	<input type="submit">
   </form>
+ 
 
+<h2>Minu huvialad</h2>
+<form> 
+ <?php if(isset($add_user_interest_response->error)): ?>
+ 
+ <p style="color:red;">
+ <?=$add_user_interest_response->error->message;?>
+ </p>
+ 
+ <?php elseif(isset($add_user_interest_response->success)): ?>
+ <p style="color:green;">
+ <?=$add_user_interest_response->success->message;?>
+ 
+ </p>
+ <?php endif; ?>  
+ <form> 
+   <?=$InterestManager->createDropdown();?>
+ <input value="lisa" type="submit">
+</form>
+
+<h2>Loetelu</h2>
+<?=$InterestManager->getUserInterests();?>
 
